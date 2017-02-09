4a92de7d251ab15b7bf9928762e074a27f68cfb8
Rob Schwartz <robertschwar@gmail.com>
2017-02-09 09:02:29 -0600

    Based off of happy_seed: base, splash, devise, facebook, googleoauth included

---
+++ app/models/user.rb
@@ -0,0 +1,34 @@
+class User < ApplicationRecord
+  attr_accessor :oauth_callback
+  attr_accessor :current_password
+    
+  validates_presence_of   :email, if: :email_required?
+  validates_uniqueness_of :email, allow_blank: true, if: :email_changed?
+  validates_format_of     :email, with: Devise.email_regexp, allow_blank: true, if: :email_changed?
+
+  validates_presence_of     :password, if: :password_required?
+  validates_confirmation_of :password, if: :password_required?
+  validates_length_of       :password, within: Devise.password_length, allow_blank: true
+
+  def password_required?
+    return false if email.blank? || !email_required?
+    !persisted? || !password.nil? || !password_confirmation.nil?
+  end
+
+  def email_required?
+    @oauth_callback != true
+  end
+  has_many :identities, dependent: :destroy
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :omniauthable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable
+  def facebook
+    identities.where( :provider => "facebook" ).first
+  end
+
+  def facebook_client
+    @facebook_client ||= Facebook.client( access_token: facebook.accesstoken )
+  end
+
+end
